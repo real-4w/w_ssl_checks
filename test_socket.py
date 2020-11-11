3a742a95590364157ecf7762b81ddf652f416272
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-11-12 08:53:20 +1300

    WIP

--- test_socket.py
+++ test_socket.py
@@ -1,8 +1,13 @@
 from urllib.request import Request, urlopen, ssl, socket
 from urllib.error import URLError, HTTPError
 import json
+
+def jprint(obj):                                            # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=8)
+    print(text)
+
 #some site without http/https in the path
-base_url = 'CHANGE_ME_TO_YOUR_SITE'
+base_url = 'myfootprint.co.nz'
 port = '443'
 
 hostname = base_url
@@ -12,6 +17,6 @@ with socket.create_connection((hostname, port)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         print(ssock.version())
         data = json.dumps(ssock.getpeercert())
-        # print(ssock.getpeercert())
+        #print(ssock.getpeercert())
 
 print (data)
